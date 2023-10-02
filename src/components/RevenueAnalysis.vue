5289a214cc332085f5b1067ac156ea8b19f7a375
Anns Mustafa <anns.mustafa191@gmail.com>
2023-10-02 13:33:05 +0500

    basic structure created for demo project

---
+++ src/components/RevenueAnalysis.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { ref, computed, watch } from 'vue';
+import { Line } from 'vue-chartjs';
+export default class RevenueAnalysis extends Vue {
+
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
