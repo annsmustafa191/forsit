976388c08e3fe04d3111ea5d4e3c6c63974f08a8
Anns Mustafa <anns.mustafa191@gmail.com>
2023-10-02 13:35:44 +0500

    linter fix

--- src/components/RevenueAnalysis.vue
+++ src/components/RevenueAnalysis.vue
@@ -1,14 +1,10 @@
-<template>
-  
-</template>
+<template></template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { ref, computed, watch } from 'vue';
-import { Line } from 'vue-chartjs';
-export default class RevenueAnalysis extends Vue {
-
-}
+import { ref, computed, watch } from "vue";
+import { Line } from "vue-chartjs";
+export default class RevenueAnalysis extends Vue {}
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
